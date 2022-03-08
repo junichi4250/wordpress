de2eee4715f19441347a9270b5df77a86acbe74e
katojun <jkky0225@gmail.com>
2022-03-08 21:47:38 +0900

    add:worksページページネーション

--- public/wp-content/themes/twentytwenty/page-works.php
+++ public/wp-content/themes/twentytwenty/page-works.php
@@ -4,46 +4,59 @@ Template Name: works
 */
 ?>
 <?php get_header(); ?>
-    <main id="wrapper" class="wrapper">
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<?php if( function_exists("the_pagination") ) the_pagination(); ?>
-<?php
-$paged = (int) get_query_var('paged');
-$args = array(
-	'posts_per_page' => 3,
-	'paged' => $paged,
-	'orderby' => 'post_date',
-	'order' => 'DESC',
-	'post_type' => 'post',
-	'post_status' => 'publish'
-);
-$the_query = new WP_Query($args);
-
-if ( $the_query->have_posts() ) :
-	while ( $the_query->have_posts() ) : $the_query->the_post();
-		get_template_part( 'content', get_post_format() );
-	endwhile;
-else:
-	get_template_part( 'content', 'none' );
-endif;
-
-if ($the_query->max_num_pages > 1) {
-	echo paginate_links(array(
-		'base' => get_pagenum_link(1) . '%_%',
-		'format' => 'page/%#%/',
-		'current' => max(1, $paged),
-		'total' => $the_query->max_num_pages
-	));
-}
-
-wp_reset_postdata();
-?>
-<?php // ブログ記事を表示する end ?>
-    </main>
+<!-- 記事ページ -->
+<main>
+    <section class="p-works">
+        <div class="works">
+            <ul class="works-item">
+                <?php
+                    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                    $the_query = new WP_Query( array(
+                    'post_status' => 'publish',
+                    'post_type' => 'post', //　ページの種類（例、page、post、カスタム投稿タイプ名）
+                    'paged' => $paged,
+                    'posts_per_page' => 3, // 表示件数
+                    'orderby'     => 'date',
+                    'order' => 'DESC'
+                    ) );
+                    if ($the_query->have_posts()) :
+                        while ($the_query->have_posts()) : $the_query->the_post();
+                    ?>
+                <?php
+                    echo the_time('Y.m.d')
+                ?>
+                <?php the_category(); ?>
+                <?php
+                endwhile;
+                else:
+                    echo '<div><p>ありません。</p></div>';
+                endif;
+                ?>
+            </ul>
+        </div>
+        <div class="pnavi">
+            <?php //ページリスト表示処理
+                global $wp_rewrite;
+                $paginate_base = get_pagenum_link(1);
+                if(strpos($paginate_base, '?') || !$wp_rewrite->using_permalinks()){
+                    $paginate_format = '';
+                    $paginate_base = add_query_arg('paged','%#%');
+                }else{
+                    $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
+                    user_trailingslashit('page/%#%/','paged');
+                    $paginate_base .= '%_%';
+                }
+                echo paginate_links(array(
+                    'base' => $paginate_base,
+                    'format' => $paginate_format,
+                    'total' => $the_query->max_num_pages,
+                    'mid_size' => 1,
+                    'current' => ($paged ? $paged : 1),
+                    'prev_text' => '< 前へ',
+                    'next_text' => '次へ >',
+                ));
+            ?>
+        </div>
+    </section>
+</main>
 <?php get_footer(); ?>
